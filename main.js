66116f862c833612e771663a88ac3b63d7189b88
ghitaeddarrie <ghitaeddarrie15@gmail.com>
2023-10-20 19:55:56 +0100

    calculater

---
+++ main.js
@@ -0,0 +1,57 @@
+class calculator{
+    constructor(perviousOperandTextElement,currentOperandTextElement ){
+        this.perviousOperandTextElement = currentOperandTextElement
+        this.perviousOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+
+
+    clear(){
+       this.currentOperand = ''
+       this.currentOperand = ''
+       this.currentOperand = ''
+    }
+
+    delete(){
+
+    }
+
+    appendNumber(Number){
+
+    }
+
+    chooseOperation(Operation){
+
+    }
+
+    compute(){
+
+    }
+
+    updateDisplay(){
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const numberButtons = document.querySelectorAll('[dara-number]')
+const operationButtons = document.querySelectorAll('[dara-operation]')
+const equalsButton = document.querySelectorAll('[dara-equals]')
+const deletButton = document.querySelectorAll('[dara-delete]')
+const allCleanButton = document.querySelectorAll('[dara-all-Clean]')
+const perviousOperandTextElement = document.querySelectorAll('[dara-pervious-Operand]')
+const currentOperandTextElement = document.querySelectorAll('[dara-current-Operand]')
+
